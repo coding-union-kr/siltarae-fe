560726bb70c26b9d8c81e35944f2bc7213a598dd
seovee <jinseob102@naver.com>
2023-12-08 19:18:08 +0900

    chore: tailwind 글꼴 추가, components폴더 경로 수정 등

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,17 +3,16 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // components 기본 경로 수정
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    fontFamily: { pretendard: ["Pretendard-Regular"] },
   },
   plugins: [require("daisyui")],
 };
