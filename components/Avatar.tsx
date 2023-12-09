cef45267d7248a781552690840de814993e7b223
cottonpup <younggyoung.lee1@gmail.com>
2023-12-10 01:50:15 +0900

    feat: 디폴트 폰트어썸 프로필 추가

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -4,19 +4,24 @@ import React from "react";
 // 아직 API 서버가 없으므로 임의적으로 img 태그를 사용하겠습니다!
 // https://nextjs.org/docs/messages/next-image-unconfigured-host
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 interface AvatarProps {
   size?: string;
+  userImageUrl?: string;
 }
 
-function Avatar({ size = "w-24" }: AvatarProps) {
+function Avatar({ size = "w-24", userImageUrl = "" }: AvatarProps) {
   return (
-    <div className="flex items-center">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-        alt="Profile"
-        className={`avatar ${size} rounded-full shadow-md shadow-slate-500`}
-      />
+    <div
+      className={`flex items-center avatar ${size} rounded-full shadow-md shadow-slate-500`}
+    >
+      {userImageUrl ? (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img src={userImageUrl} alt="Profile" />
+      ) : (
+        <FontAwesomeIcon icon="fa-solid fa-user" />
+      )}
     </div>
   );
 }
