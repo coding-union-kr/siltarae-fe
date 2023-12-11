b5fc7ce64a7e9a2da9f6ca635e00cb3869eb82a2
cottonpup <younggyoung.lee1@gmail.com>
2023-12-11 17:15:25 +0900

    feat: ContentCard 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
-import ProfileAvatar from "@/components/ProfileAvatar";
 import React from "react";
 
 export default function Home() {
   return (
     <main>
-      <ProfileAvatar />
     </main>
   );
 }
