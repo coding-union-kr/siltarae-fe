0ade7c43e905f8808085c6c0e9f657e00e1d355c
Younggyoung Lee <67526014+cottonpup@users.noreply.github.com>
2023-12-13 14:37:19 +0900

    [Feat] Comment 구현 완료 (#24)
    * feat: Comment 구현
    
    * fix: 머지 충돌

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,5 @@
-import CommentInput from "@/components/CommentInput";
 import React from "react";
 
 export default function Home() {
-  return (
-    <main>
-      <CommentInput />
-    </main>
-  );
+  return <main />;
 }
