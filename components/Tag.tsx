3dde17e29d4fd0bd0709d7de295705720cd346d9
seovee <jinseob102@naver.com>
2023-12-13 13:36:10 +0900

    feat: Tag onClick 프로퍼티 추가

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -3,12 +3,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 export default function Tag() {
+  const onClick = () => {
+    console.log("삭제 기능을 추가해주세요");
+  };
+
   return (
     <div className="flex flex-row flex-nowrap gap-3">
-      {["포캣몬", "마스터", "될래요"].map((tagTitle) => (
-        <div className="bg-[#9CC490] flex gap-2 items-center w-30 px-4 py-1 rounded-[20px] text-white text-sm font-semibold">
+      {["포캣몬", "마스터", "될래요"].map((tagTitle, i) => (
+        <div
+          key={i}
+          className="bg-[#9CC490] flex gap-2 items-center w-30 px-4 py-1 rounded-[20px] text-white text-sm font-semibold"
+        >
           <span>{tagTitle}</span>
-          <div className="cursor-pointer">
+          <div onClick={onClick} className="cursor-pointer">
             <FontAwesomeIcon icon={faX} size="xs" />
           </div>
         </div>
