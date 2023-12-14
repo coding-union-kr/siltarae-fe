242afc98f154ecb38a6ec272bfa1dda59331c3b1
cottonpup <younggyoung.lee1@gmail.com>
2023-12-14 23:32:08 +0900

    fix: navBar 사이즈 변경 및 헤더 z-index 값 추가

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,7 +9,7 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <section className="fixed bottom-0 h-[10vh] w-full bg-white backdrop-blur-md">
+    <section className="fixed bottom-0 h-[10vh] w-[500px] bg-white backdrop-blur-md">
       <div className="h-full flex flex-row justify-evenly items-center">
         <Link href="/" className="flex flex-col items-center">
           <FontAwesomeIcon icon={faHouse} size="2x" />
