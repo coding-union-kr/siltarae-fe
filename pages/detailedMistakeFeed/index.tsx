68a42ae33f1760857de65f2d668f580c169ef1dd
seovee <jinseob102@naver.com>
2023-12-14 22:50:45 +0900

    chore: detailedMistakeFeed CSS 수정

--- pages/detailedMistakeFeed/index.tsx
+++ pages/detailedMistakeFeed/index.tsx
@@ -25,7 +25,7 @@ export default function DetailedMistakeFeed() {
         </div>
       </section>
       <ContentPage />
-      <section>
+      <section className="border-t-[0.3px] border-[#DFE2E9]">
         <span className="block px-5 pt-5 font-bold text-[#856E69] text-[20px]">
           댓글 17개
         </span>
