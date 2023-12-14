364533d7c61d784495b95d130cf47ad5fc477803
cottonpup <younggyoung.lee1@gmail.com>
2023-12-14 22:33:13 +0900

    fix: 레이아웃 수정

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Header from "./Header";
+import NavBar from "./NavBar";
 
 function Layout(props: { children: React.ReactNode }) {
   const { children } = props;
   return (
     <div id="container" className="h-screen flex justify-center">
-      <div className="w-[500px] max-w-[500px]">{children}</div>
+      <div className="w-[500px] max-w-[500px]">
+        <Header />
+        {children}
+        <NavBar />
+      </div>
     </div>
   );
 }
