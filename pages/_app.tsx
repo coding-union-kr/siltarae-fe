d789bfa30a2e7183fdccf8d6ce79cbc38052f41a
Younggyoung Lee <67526014+cottonpup@users.noreply.github.com>
2023-12-14 15:34:18 +0900

    feat: 레이아웃 구현 (#33)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import Layout from "@/layout/Layout";
 
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
