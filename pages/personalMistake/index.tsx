948b521b9d86ea4d9069f0a1845103bb0e3fc327
seovee <jinseob102@naver.com>
2023-12-18 19:31:35 +0900

    fix: PersonalMistake페이지 CSS 틀어지는 오류 해결

--- pages/personalMistake/index.tsx
+++ pages/personalMistake/index.tsx
@@ -5,33 +5,36 @@ import React from "react";
 
 export default function PersonalMistake() {
   return (
-    <div className="bg-[#FDF8F3] px-5 pb-5">
-      <div className="flex flex-col gap-5">
+    <div className="bg-[#FDF8F3]">
+      <div className="flex flex-col items-center gap-2">
         <Tag />
-        <ContentCard
-          author="람쥐"
-          content="엄청 피곤해여.."
-          comments={12352}
-          like={34352}
-        />
-        <ContentCard
-          author="진섭"
-          content="일찍 운동도 갔다오고, 아침 카페가서 한잔하고 여유있게 하루를 시작했습니다."
-          comments={1000}
-          like={1000}
-        />
-        <ContentCard
-          author="매니"
-          content="점심 순대국밥!"
-          comments={30}
-          like={35}
-        />
-        <ContentCard
-          author="파덕"
-          content="졸립니다..."
-          comments={50}
-          like={50}
-        />
+        {/* CHECK: div를 씌우지 않으면 layer가 틀어짐, 이유는 모르겠음.. */}
+        <div>
+          <ContentCard
+            author="람쥐"
+            content="엄청 피곤해여.."
+            comments={12352}
+            like={34352}
+          />
+          <ContentCard
+            author="진섭"
+            content="일찍 운동도 갔다오고, 아침 카페가서 한잔하고 여유있게 하루를 시작했습니다."
+            comments={1000}
+            like={1000}
+          />
+          <ContentCard
+            author="매니"
+            content="점심 순대국밥!"
+            comments={30}
+            like={35}
+          />
+          <ContentCard
+            author="파덕"
+            content="졸립니다..."
+            comments={50}
+            like={50}
+          />
+        </div>
       </div>
       <AddPostButton />
     </div>
