4b1b809421e6c653e84d11a2bd028a462491ed2a
seovee <jinseob102@naver.com>
2023-12-18 14:33:58 +0900

    feat: map 동작에 key 프로퍼티 추가

--- pages/tagList/index.tsx
+++ pages/tagList/index.tsx
@@ -5,19 +5,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
 
 const exData = [
-  "태그1",
-  "태그2",
-  "태그3",
-  "태그4",
-  "태그5",
-  "태그6",
-  "태그7",
-  "태그8",
-  "태그9",
-  "서비",
-  "람쥐님",
-  "파덕님",
-  "매니님",
+  { num: 1, title: "태그1" },
+  { num: 2, title: "태그2" },
+  { num: 3, title: "태그3" },
+  { num: 4, title: "태그4" },
+  { num: 5, title: "태그5" },
+  { num: 6, title: "태그6" },
+  { num: 7, title: "태그7" },
+  { num: 8, title: "태그8" },
+  { num: 9, title: "태그9" },
+  { num: 10, title: "서비" },
+  { num: 11, title: "람쥐님" },
+  { num: 12, title: "파덕님" },
+  { num: 13, title: "매니님" },
 ];
 
 export default function TagList() {
@@ -31,11 +31,12 @@ export default function TagList() {
       <ul className="bg-white h-auto text-2xl rounded-[10px] overflow-hidden shadow-md">
         {exData.map((data) => (
           <motion.li
+            key={data.num}
             whileHover={{ backgroundColor: "#DDDEDF" }}
             className="p-5 flex flex-row justify-between"
             onClick={selectTag}
           >
-            <p>{data}</p>
+            <p>{data.title}</p>
             <div>
               <FontAwesomeIcon icon={faEllipsis} size="sm" />
             </div>
