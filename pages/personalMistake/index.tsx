c13fa9d49ff667c11274ccfc372e25e83bfbf24e
seovee <jinseob102@naver.com>
2023-12-18 16:46:18 +0900

    feat: personalMistake 페이지 임시 props 추가

--- pages/personalMistake/index.tsx
+++ pages/personalMistake/index.tsx
@@ -8,12 +8,30 @@ export default function PersonalMistake() {
     <div className="bg-[#FDF8F3] px-5 pb-5">
       <div className="flex flex-col gap-5">
         <Tag />
-        <ContentCard />
-        <ContentCard />
-        <ContentCard />
-        <ContentCard />
-        <ContentCard />
-        <ContentCard />
+        <ContentCard
+          author="람쥐"
+          content="엄청 피곤해여.."
+          comments={12352}
+          like={34352}
+        />
+        <ContentCard
+          author="진섭"
+          content="일찍 운동도 갔다오고, 아침 카페가서 한잔하고 여유있게 하루를 시작했습니다."
+          comments={1000}
+          like={1000}
+        />
+        <ContentCard
+          author="매니"
+          content="점심 순대국밥!"
+          comments={30}
+          like={35}
+        />
+        <ContentCard
+          author="파덕"
+          content="졸립니다..."
+          comments={50}
+          like={50}
+        />
       </div>
       <AddPostButton />
     </div>
