72116f3a49e2d5c81cee5c84f8728f9822732136
seovee <jinseob102@naver.com>
2023-12-18 19:30:24 +0900

    fix: fixed 요소 가운데 정렬 추가

--- components/AddPostButton.tsx
+++ components/AddPostButton.tsx
@@ -5,10 +5,11 @@ import React from "react";
 
 function AddPostButton() {
   return (
-    <div className="fixed bottom-16 w-full max-w-[500px]">
+    // fixed를 가운데 정렬로 사용하는 방법
+    <div className="fixed bottom-16 left-0 right-0 mx-auto w-full max-w-[500px]">
       <Link
         href="/"
-        className="absolute bottom-5 right-5 h-[3.5rem] w-[3.5rem] flex justify-center items-center rounded-full bg-[#9CC491] shadow-md hover:scale-[1.1]"
+        className="absolute bottom-6 right-5 h-[3.5rem] w-[3.5rem] flex justify-center items-center rounded-full bg-[#9CC491] shadow-md"
       >
         <FontAwesomeIcon icon={faPlus} size="2xl" color="white" />
       </Link>
