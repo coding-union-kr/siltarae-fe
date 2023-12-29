c48e337ac55da27ea720476f78e10efbca4fb211
cottonpup <younggyoung.lee1@gmail.com>
2023-12-29 12:16:11 +0900

    fix: Link 태그 위치 변경

--- components/ContentCard.tsx
+++ components/ContentCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { likePost } from "@/api/mistakeApi";
 import { useMutation } from "@tanstack/react-query";
+import Link from "next/link";
 import Avatar from "./Avatar";
 import LikeButton from "./LikeButton";
 
@@ -24,24 +25,25 @@ function ContentCard({
   });
 
   return (
-    <article className="card w-[20rem] xs:w-[28rem] bg-white text-neutral-content shadow-md shadow-slate-300 xs:p-6 p-5 my-3">
-      <section className="flex items-center mb-3">
-        <Avatar />
-        <h3 className="ml-2 text-base xs:text-lg text-[#856E69] font-bold">
-          {author}
-        </h3>
-      </section>
+    <Link href={`/detailedMistakeFeed/${id}`}>
+      <article className="card w-[20rem] xs:w-[28rem] bg-white text-neutral-content shadow-md shadow-slate-300 xs:p-6 p-5 my-3">
+        <section className="flex items-center mb-3">
+          <Avatar />
+          <h3 className="ml-2 text-base xs:text-lg text-[#856E69] font-bold">
+            {author}
+          </h3>
+        </section>
 
-      <p className="text-sm xs:text-base text-[#5C4F4D] leading-normal break-keep">
-        {content}
-      </p>
-      <section className="flex items-center justify-between text-sm xs:text-base mt-3 text-slate-400">
-        {/* FIXME:  href 나중에 변경 */}
-        댓글 {comments} 개
-        <LikeButton count={like} onLikeClick={mutate} />
-      </section>
-      <div />
-    </article>
+        <p className="text-sm xs:text-base text-[#5C4F4D] leading-normal break-keep">
+          {content}
+        </p>
+        <section className="flex items-center justify-between text-sm xs:text-base mt-3 text-slate-400">
+          댓글 {comments} 개
+          <LikeButton count={like} onLikeClick={mutate} />
+        </section>
+        <div />
+      </article>
+    </Link>
   );
 }
 
