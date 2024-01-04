d4d2dd3a7264e6f7e7d8b282426c918b5effc678
김진섭 <18073169+seovee@users.noreply.github.com>
2024-01-04 16:33:23 +0900

    feat: 댓글 api 기능 연결 (#85)
    * feat: comment 조회하는 api 구현 완료
    
    * fix: key값이 부여되지 않는 오류 수정
    
    * feat: 댓글 추가(post)하기 api 생성
    
    * fix: 댓글 조회 CSS 수정
    
    * feat: 댓글 추가기능 구현 완료
    
    * chore: 오타 수정
    
    * feat: 댓글 삭제(delete)기능 구현 완료
    
    * feat: 댓글 api에 env 파일 적용
    
    * chore: 함수명 통일성을 위해 변경

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -1,18 +1,87 @@
 import React from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { deleteComments, fetchComments } from "@/api/commentApi";
+import { AxiosError } from "axios";
+import { useRouter } from "next/router";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Avatar from "./Avatar";
 
+type CommentDataType = {
+  memberId: number;
+  memberName: string;
+  commentId: number;
+  commentContent: string;
+};
+
 function Comment() {
+  const queryCilent = useQueryClient();
+  const router = useRouter();
+  const { id } = router.query;
+
+  const {
+    data: comments,
+    isPending,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["comments_get", id],
+    queryFn: () => fetchComments(10, 0, id as string),
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: (commentId: number) => deleteComments(commentId),
+    onSuccess: () => {
+      // eslint-disable-next-line no-console
+      console.log("삭제 성공");
+    },
+    onError: () => {
+      // eslint-disable-next-line no-console
+      console.log("실패");
+    },
+    onSettled: () => {
+      queryCilent.invalidateQueries();
+    },
+  });
+  const onClickDelete = (commentId: number) => {
+    mutate(commentId);
+  };
+
   return (
-    <div className="flex flex-row gap-2 items-start px-8 py-5">
-      <Avatar />
-      <div className="ml-2 mt-1">
-        <h3 className="text-lg text-[#856E69] font-bold mb-2">고라파덕</h3>
-        <p className="text-base text-[#5C4F4D] leading-normal break-keep text-justify max-w-[500px] ">
-          로렘 입숨은 출판이나 그래픽 디자인 분야에서 폰트, 타이포그래피,
-          레이아웃 같은 그래픽 요소나 시각적 연출을 보여줄 때 사용하는 표준
-          채우기 텍스트
-        </p>
-      </div>
+    <div>
+      {isPending && (
+        <span className="loading loading-dots loading-lg text-secondary" />
+      )}
+      {isError && (
+        <span>
+          {error instanceof AxiosError
+            ? error?.message
+            : "댓글을 불러오는데 실패했습니다. 새로고침 해주세요."}
+        </span>
+      )}
+      {comments?.map((comment: CommentDataType) => (
+        <div
+          key={comment.commentId}
+          className="relative flex flex-row gap-2 items-center px-8 py-5"
+        >
+          <Avatar />
+          <div className="ml-2 mt-1">
+            <h3 className="text-lg text-[#856E69] font-bold mb-2">
+              {comment.memberName}
+            </h3>
+            <p className="text-base text-[#5C4F4D] leading-normal break-keep text-justify max-w-[500px]">
+              {comment.commentContent}
+            </p>
+          </div>
+          <FontAwesomeIcon
+            icon={faX}
+            size="xs"
+            color="#856E69"
+            onClick={() => onClickDelete(comment.commentId)}
+            className="absolute right-10 cursor-pointer hover:text-[#EBDDCC]"
+          />
+        </div>
+      ))}
     </div>
   );
 }
