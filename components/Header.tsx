f59cc09d1b758dce2fea953ab307e5beeb1d1f3b
김진섭 <18073169+seovee@users.noreply.github.com>
2024-01-05 13:50:02 +0900

    fix: 스크롤바 너비 생성으로 인해 전체 Width값 변화로 인한 레이아웃 오류 수정 (#89)
    * fix: fixed 요소 가운데 정렬 추가
    
    * fix: PersonalMistake페이지 CSS 틀어지는 오류 해결
    
    * fix: 스크롤 너비로 인한 레이아웃 오류 CSS 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <section className="fixed top-0 backdrop-blur-md h-16 w-full max-w-[500px] border-b border-b-gray-300 z-50">
+    <section className="fixed top-0 left-0 right-0 mx-auto backdrop-blur-md h-16 w-full max-w-[500px] border-b border-b-gray-300 z-50">
       <div className="flex flex-row h-full justify-between items-center px-5">
         <div>
           <Link href="/">
