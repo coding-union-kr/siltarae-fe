7a3c602b9333f19af19e94ee9300018a53b92439
cottonpup <younggyoung.lee1@gmail.com>
2024-01-10 13:59:19 +0900

    fix: LikeButton 리팩토링

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,46 +1,24 @@
 import { faHeart as fasHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
+import React from "react";
 
 interface LikeButtonProps {
   count: number;
-  onLikeClick: () => void;
+  onLikeClick: (e: React.MouseEvent<SVGSVGElement>) => void;
+  isLiked: boolean;
 }
 
-function LikeButton({ count, onLikeClick = () => {} }: LikeButtonProps) {
-  const [isLike, setIsLike] = useState(false);
-  // const [count, setCount] = useState(325);
-
-  const toggleLikeButton = (e: React.MouseEvent<SVGSVGElement>) => {
-    e.preventDefault();
-    if (!isLike) {
-      // setCount((prev) => prev + 1);
-      onLikeClick();
-      setIsLike(true);
-    } else {
-      // setCount((prev) => prev - 1);
-      setIsLike(false);
-    }
-  };
+function LikeButton({ count, onLikeClick, isLiked }: LikeButtonProps) {
   return (
     <div className="select-none">
       <span className="mr-2 ">{count}</span>
-      {isLike ? (
-        <FontAwesomeIcon
-          onClick={toggleLikeButton}
-          icon={fasHeart}
-          size="xl"
-          className="cursor-pointer text-[#F3685F]"
-        />
-      ) : (
-        <FontAwesomeIcon
-          onClick={toggleLikeButton}
-          icon={farHeart}
-          size="xl"
-          className="cursor-pointer text-[#F3685F]"
-        />
-      )}
+      <FontAwesomeIcon
+        onClick={onLikeClick}
+        icon={isLiked ? fasHeart : farHeart}
+        size="xl"
+        className="cursor-pointer text-[#F3685F]"
+      />
     </div>
   );
 }
