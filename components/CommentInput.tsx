c6dfb4f5447fe21092d54458b5355d2fe9961d96
seovee <jinseob102@naver.com>
2024-01-12 11:55:58 +0900

    feat: 댓글 추가 Enter 적용 & PopUp setTimeout 적용

--- components/CommentInput.tsx
+++ components/CommentInput.tsx
@@ -7,12 +7,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Avatar from "./Avatar";
 
 function CommentInput() {
+  const [toast, setToast] = useState(false);
   const queryCilent = useQueryClient();
   const router = useRouter();
   const { id } = router.query;
   const [content, setContent] = useState("");
   // 댓글 추가기능
-  const { mutate, isPending, isError, error, isSuccess } = useMutation({
+  const { mutate, isError, error, isSuccess } = useMutation({
     mutationFn: () => createComments(id as string, content),
     onSuccess: () => {
       setContent("");
@@ -27,25 +28,43 @@ function CommentInput() {
     setContent(e.target.value);
   };
 
+  const handleClick = () => {
+    setToast(true);
+    mutate();
+    // Toast 사라지게 하는 코드
+    setTimeout(() => {
+      setToast(false);
+    }, 3000);
+  };
+
+  // 댓글 엔터기능
+  const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setToast(true);
+      mutate();
+      // Toast 사라지게 하는 코드
+      setTimeout(() => {
+        setToast(false);
+      }, 3000);
+    }
+  };
+
   return (
     <div className="fixed bottom-0 border-t-[0.1px] border-[#DFE2E9] py-5 px-5 w-full max-w-[500px] backdrop-blur-md">
-      <div className="absolute flex justify-center items-center right-0 left-0 mx-auto w-auto top-[-50px]">
-        {isPending && (
-          <span className="flex justify-center items-center bg-[#9CC490]/60 w-[70%] h-16 rounded-xl">
-            Loading...
-          </span>
-        )}
-        {isSuccess && (
-          <span className="flex justify-center items-center bg-[#9CC490]/60 w-[70%] h-16 rounded-xl">
-            댓글이 성공적으로 달렸습니다.
-          </span>
-        )}
-        {isError && (
-          <span className="flex justify-center items-center bg-[#9CC490]/60 w-[70%] h-16 rounded-xl">
-            댓글달기 실패했습니다 : {error.message}
-          </span>
-        )}
-      </div>
+      {toast && (
+        <div className="absolute flex justify-center items-center right-0 left-0 mx-auto w-auto top-[-50px]">
+          {isSuccess && (
+            <span className="flex justify-center items-center bg-[#9CC490]/60 w-[70%] h-16 rounded-xl">
+              댓글이 성공적으로 달렸습니다.
+            </span>
+          )}
+          {isError && (
+            <span className="flex justify-center items-center bg-[#9CC490]/60 w-[70%] h-16 rounded-xl">
+              댓글달기 실패했습니다 : {error.message}
+            </span>
+          )}
+        </div>
+      )}
       <div className="flex items-center">
         <Avatar />
         <input
@@ -54,12 +73,13 @@ function CommentInput() {
           onChange={handleContentChange}
           placeholder="댓글을 입력해주세요."
           className="flex-1 ml-3 input input-bordered border-2 border-[#9CC490] focus:border-[#9CC490] rounded-2xl"
+          onKeyUp={handleKeyUp}
         />
         <FontAwesomeIcon
           icon={faCircleCheck}
           size="xl"
           className="absolute right-8 text-[#9CC490] cursor-pointer"
-          onClick={() => mutate()}
+          onClick={handleClick}
         />
       </div>
     </div>
