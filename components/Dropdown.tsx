7186c9f0de4519a046d46a89fd09a7691d7172c3
seovee <jinseob102@naver.com>
2024-01-22 15:43:11 +0900

    feat: Dropdown 컴포넌트 생성 및 구현

---
+++ components/Dropdown.tsx
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+export default function Dropdown() {
+  return (
+    // daisy UI - 드롭다운 적용
+    <div className="dropdown dropdown-end">
+      <FontAwesomeIcon
+        icon={faEllipsis}
+        style={{ color: "#856E69" }}
+        role="button"
+        size="xl"
+        className="bg-transparent border-none hover:scale-[1.1] active:scale-[0.9] hover:transition-transform active:transition-transform"
+        tabIndex={0}
+      />
+      <ul className="dropdown-content p-3 w-28 h-auto z-[1] menu bg-base-100 rounded-box flex-column align-center justify-evenly font-bold text-[#856E69]">
+        <li className="text-center p-2 rounded-box hover:bg-[#EBE2DD] hover:transition-colors cursor-pointer">
+          수정 하기
+        </li>
+        <li className="text-center p-2 rounded-box hover:bg-[#EBE2DD] hover:transition-colors cursor-pointer">
+          삭제 하기
+        </li>
+      </ul>
+    </div>
+  );
+}
--- components/PostHeader.tsx
+++ components/PostHeader.tsx
@@ -1,26 +1,24 @@
-import { faAngleLeft, faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import React from "react";
+import Dropdown from "./Dropdown";
 
 export default function PostHeader() {
   return (
     <section className="fixed top-0 bg-white h-16 p-5 w-full max-w-[500px] border-b border-b-gray-300 z-50">
       <div className="flex flex-row justify-between items-center h-full">
-        <Link href="/">
+        <Link
+          href="/"
+          className="hover:scale-[1.1] active:scale-[0.9] hover:transition-transform active:transition-transform"
+        >
           <FontAwesomeIcon
             icon={faAngleLeft}
             size="lg"
             style={{ color: "#856E69" }}
           />
         </Link>
-        <div>
-          <FontAwesomeIcon
-            icon={faEllipsis}
-            size="xl"
-            style={{ color: "#856E69" }}
-          />
-        </div>
+        <Dropdown />
       </div>
     </section>
   );
