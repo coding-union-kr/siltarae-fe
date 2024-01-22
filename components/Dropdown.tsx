124876411904c8dc4f66f0fa0d18c45391435df7
seovee <jinseob102@naver.com>
2024-01-22 15:59:05 +0900

    chore: CSS 살짝 수정

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -14,12 +14,12 @@ export default function Dropdown() {
         className="bg-transparent border-none hover:scale-[1.1] active:scale-[0.9] hover:transition-transform active:transition-transform"
         tabIndex={0}
       />
-      <ul className="dropdown-content p-3 w-28 h-auto z-[1] menu bg-base-100 rounded-box flex-column align-center justify-evenly font-bold text-[#856E69]">
-        <li className="text-center p-2 rounded-box hover:bg-[#EBE2DD] hover:transition-colors cursor-pointer">
-          수정 하기
+      <ul className="dropdown-content p-3 w-20 h-auto z-[1] menu bg-base-100 rounded-box flex-column align-center justify-evenly font-bold text-[#856E69]">
+        <li className="text-center p-2 rounded-box hover:bg-[#EBE2DD] hover:transition-colors cursor-not-allowed">
+          수정
         </li>
         <li className="text-center p-2 rounded-box hover:bg-[#EBE2DD] hover:transition-colors cursor-pointer">
-          삭제 하기
+          삭제
         </li>
       </ul>
     </div>
