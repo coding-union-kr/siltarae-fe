8decc80f4ea9f3b6d50d300f65156e1614a0a953
cottonpup <younggyoung.lee1@gmail.com>
2024-01-29 14:41:44 +0900

    fix: router import 하고 뒤로가기 버튼 수정

--- components/PostHeader.tsx
+++ components/PostHeader.tsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
-import Link from "next/link";
 import React from "react";
+import { useRouter } from "next/router";
 import Dropdown from "./Dropdown";
 
 export default function PostHeader() {
@@ -9,16 +9,16 @@ export default function PostHeader() {
   return (
     <section className="fixed top-0 bg-white h-16 p-5 w-full max-w-[500px] border-b border-b-gray-300 z-50">
       <div className="flex flex-row justify-between items-center h-full">
-        <Link
-          href="/"
-          className="hover:scale-[1.2] active:scale-[0.9] hover:transition-transform active:transition-transform"
+        <div
+          onClick={() => router.back()}
+          className="hover:scale-[1.2] active:scale-[0.9] hover:transition-transform active:transition-transform cursor-pointer"
         >
           <FontAwesomeIcon
             icon={faAngleLeft}
             size="lg"
             style={{ color: "#856E69" }}
           />
-        </Link>
+        </div>
         <Dropdown />
       </div>
     </section>
