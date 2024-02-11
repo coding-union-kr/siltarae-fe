c26312bb5d633ed5d9dab7adb1601db9a331db1a
seovee <jinseob102@naver.com>
2024-02-11 21:16:04 +0900

    fix: 개발환경, 배포환경 env 설정을 위한 api 수정

--- api/authApi.ts
+++ api/authApi.ts
@@ -1,7 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 import api from "./api";
 
+const redirectUri = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URL;
+
 export const loginWithGoogle = async (authCode: string | null) => {
-  const response = await api.post("/login/google", { authCode });
+  const response = await api.post("/login/google", { authCode, redirectUri });
   return response.data;
 };
